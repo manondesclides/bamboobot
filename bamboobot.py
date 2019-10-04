b5ebd16ac0f2850177929e1d92ae92423ab19bb9
manondesclides <manon.desclides@gmail.com>
2019-10-04 17:12:52 +0200

    bamboobot !

---
+++ bamboobot.py
@@ -0,0 +1,26 @@
+
+import requests
+import re
+from telegram.ext import Updater, CommandHandler
+
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()    
+    url = contents['url']
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url) 
+
+
+def main():
+    updater = Updater('950468208:AAFagenhniR5_QnnIaPbPbiHOSBYmf1brGY')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+    
+if __name__ == '__main__':
+    main()
